3d3ab1b1ca714c33a9c58c86a779edd16daa882c
Abdon <abdonishema@gmail.com>
2024-02-28 22:37:11 +0200

    f

---
+++ javascript.js
@@ -0,0 +1,18 @@
+
+
+
+
+
+
+
+
+<!DOCTYPE html>
+<html
+  lang="en"
+ 
+
+    <div id="js-global-screen-reader-notice" class="sr-only" aria-live="polite" aria-atomic="true" ></div>
+    <div id="js-global-screen-reader-notice-assertive" class="sr-only" aria-live="assertive" aria-atomic="true"></div>
+  </body>
+</html>
+
